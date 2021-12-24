c440dac0bae980e84b8a229eb416ca42041862eb
Ahmed-Nasser-99 <forall3020@gmail.com>
2021-12-24 23:59:23 +0200

    another huge change

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listene
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
-  console.log("starting");
+  console.log("starting...");
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
   if (!process.env.NATS_CLIENT_ID)
